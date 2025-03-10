83552e67cf1c301f3427b4caa9bd4c9a4392a027
geardestroyer85 <geardestroyer85@gmail.com>
2025-03-10 14:48:54 -0400

    Configured the proxy for angular project

--- gateway/index.js
+++ gateway/index.js
@@ -4,40 +4,42 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = '172.16.101.39'
 
 // Proxy for the home app
 app.use('/home', createProxyMiddleware({
-  target: `http://localhost:${process.env.HOME_SERVICE_PORT || 4001}`,
+  target: `http://${HOST}:${process.env.HOME_APP_PORT || 4001}/home`,
   changeOrigin: true,
 }));
 
+
 // Proxy for the gosipo app
 app.use('/gosipo', createProxyMiddleware({
-  target: `http://localhost:${process.env.GOSIPO_SERVICE_PORT || 4002}`,
+  target: `http://${HOST}:${process.env.GOSIPO_SERVICE_PORT || 4002}`,
   changeOrigin: true,
 }));
 
 // Proxy for the cvallery app
 app.use('/cvallery', createProxyMiddleware({
-  target: `http://localhost:${process.env.CVALLERY_SERVICE_PORT || 4003}`,
+  target: `http://${HOST}:${process.env.CVALLERY_SERVICE_PORT || 4003}`,
   changeOrigin: true,
 }));
 
 // Proxy for the auth service
 app.use('/api/auth', createProxyMiddleware({
-  target: `http://localhost:${process.env.AUTH_SERVICE_PORT || 5001}`,
+  target: `http://${HOST}:${process.env.AUTH_SERVICE_PORT || 5001}`,
   changeOrigin: true,
 }));
 
 // Proxy for the cvallery service (backend)
 app.use('/api/cvallery', createProxyMiddleware({
-  target: `http://localhost:${process.env.CVALLERY_BACKEND_PORT || 6001}`,
+  target: `http://${HOST}:${process.env.CVALLERY_BACKEND_PORT || 6001}`,
   changeOrigin: true,
 }));
 
 // Proxy for the gosipo service (backend)
 app.use('/api/gosipo', createProxyMiddleware({
-  target: `http://localhost:${process.env.GOSIPO_BACKEND_PORT || 6002}`,
+  target: `http://${HOST}:${process.env.GOSIPO_BACKEND_PORT || 6002}`,
   changeOrigin: true,
 }));
 
